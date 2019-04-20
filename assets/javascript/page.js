e5af075e9aa259afaf9f9c18168d07f538aae7f2
Ethan <ebonsignori@gmail.com>
2019-04-20 00:40:03 -0400

    sidenav

--- assets/javascript/page.js
+++ assets/javascript/page.js
@@ -1,23 +1,44 @@
 $(document).ready(function () {
   $('.parallax').parallax()
-  $('.sidenav').sidenav()
+
+  const navButtonContainer = document.querySelector('#nav-button-container')
+  const navButton = document.querySelector('#nav-button')
 
   let animNavButtonOpen = () => {
-    console.log('open')
+    navButtonContainer.classList.remove('nav-closed')
+    navButtonContainer.classList.add('nav-open')
+    navButton.classList.add('is-active', 'sidenav-close')
   }
 
   let animNavButtonClose = () => {
-    console.log('close')
+    navButtonContainer.classList.remove('nav-open')
+    navButtonContainer.classList.add('nav-closed')
+    navButton.classList.remove('is-active', 'sidenav-close')
   }
 
-  $('.button-collapse').sideNav({
-    menuWidth: 300,
+  let options = {
     edge: 'left',
-    closeOnClick: true,
     draggable: true,
-    preventScrolling: false,
-    onOpen: animNavButtonOpen(),
-    onClose: animNavButtonClose()
-  })
+    inDuration: 1000,
+    outDuration: 1000,
+    onOpenStart: animNavButtonOpen,
+    onCloseStart: animNavButtonClose,
+    preventScrolling: false
+  }
 
+  let elems = document.querySelectorAll('.sidenav')
+  let instances = M.Sidenav.init(elems, options)
+
+  navButton.addEventListener('click', function () {
+    let elem = document.querySelector('.sidenav')
+    let instance = M.Sidenav.getInstance(elem)
+
+    if (instance.isOpen) {
+      instance.close()
+      console.log('closing')
+    } else {
+      instance.open()
+      console.log('opening')
+    }
+  })
 })
