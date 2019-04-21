ff4e76ecb1ec2c24414457615227771459b9998a
Ethan <ebonsignori@gmail.com>
2019-04-21 14:17:14 -0400

    firebase functions for contact form

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,49 @@
-const functions = require('firebase-functions');
+'use strict'
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const functions = require('firebase-functions')
+const admin = require('firebase-admin')
+const nodemailer = require('nodemailer')
+
+const email = functions.config().gmail.login
+const pass = functions.config().gmail.pass
+
+admin.initializeApp()
+
+// Function to send emails
+let sendEmail = function (message) {
+  // Init nodemailer
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: email,
+      pass: pass
+    }
+  })
+
+  // Configure emails
+  const mailOptions = {
+    from: email,
+    to: 'ebonsignori@gmail.com',
+    subject: '🦍🦐🐖🦍🦐🐖🦍🦐🐖🦍🦐🐖🦍🦐🐖',
+    text: '!' + message,
+    html: '!' + message
+  }
+
+  // Callback status
+  const getDeliveryStatus = function (error, info) {
+    if (error) {
+      return console.log(error)
+    }
+    console.log('Message sent: %s', info.messageId)
+  }
+
+  // Send email and return status
+  transporter.sendMail(mailOptions, getDeliveryStatus)
+}
+
+exports.onDataAdded = functions.database.ref('/messages/{sessionId}').onCreate(function (snap, context) {
+  const createdData = snap.val()
+  let text = createdData.mail
+
+  sendEmail(text)
+})
--- public/assets/javascript/firebase.js
+++ public/assets/javascript/firebase.js
@@ -6,5 +6,44 @@ const config = {
   projectId: 'ethan-portfoliio',
   storageBucket: 'ethan-portfolio.appspot.com',
   messagingSenderId: '728491415873'
-};
+}
 firebase.initializeApp(config)
+
+// Reference message collectionn
+const messagesRef = firebase.database().ref('messages')
+
+// Function on submit contact form
+const submitForm = (e) => {
+  e.preventDefault()
+
+  let name = getVal('name')
+  let email = getVal('email')
+  let body = getVal('message')
+
+  if (!firebase.apps.length) {
+    firebase.initializeApp(config)
+  }
+
+  // Save message
+  saveMessage(name, email, body)
+
+  // Clear form
+  document.getElementById('contact-form').reset()
+}
+
+// Listen for form submit
+$('#contact-form').on('submit', submitForm)
+
+// Get form values
+const getVal = (id) => {
+  return document.getElementById(id).value
+}
+
+// Save message to firebase
+const saveMessage = (name, email, body) => {
+  messagesRef.push().set({
+    name: name,
+    email: email,
+    body: body
+  })
+}
